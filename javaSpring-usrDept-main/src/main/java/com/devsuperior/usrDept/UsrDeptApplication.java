b255618a6525b1cbc598640b6e4bfb6aa8ea5266
ALEX ANDRADE <118372143+aoandrade-uesc@users.noreply.github.com>
2022-12-20 15:53:36 -0300

    Add files via upload
    aula 20-12

---
+++ javaSpring-usrDept-main/src/main/java/com/devsuperior/usrDept/UsrDeptApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.usrDept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsrDeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsrDeptApplication.class, args);
+	}
+
+}
---
+++ javaSpring-usrDept-main/src/test/java/com/devsuperior/usrDept/UsrDeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.usrDept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsrDeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
